8aefd47e8c042d8e57ca04cff053de58485cb43c
Supartono Efendi <supartono2002@gmail.com>
2022-06-05 11:31:33 +0700

    reverted to commit 62270a560e9b7ae369e174512a983bb4ca975d0d

--- test.php
+++ test.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>
-		<table>
-			<tr>
-				...
-			</tr>
-		</table>
-	</title>
-</head>
-<body>
 
-</body>
-</html>
-end
+<html>
+<body>
+	<ul>
+		<li class="item1"><a href=""></a></li>
+		<li class="item2"><a href=""></a></li>
+		<li class="item3"><a href=""></a></li>
+		<li class="item4"><a href=""></a></li>
+	</ul>
+	<ul id="nav">
+		<li class="item1"><a href="">Item 1</a></li>
+		<li class="item2"><a href="">Item 2</a></li>
+		<li class="item3"><a href="">Item 3</a></li>
+		<li class="item4"><a href="">Item 4</a></li>
+	</ul>
+
+	
+	</body>
+	</html>
