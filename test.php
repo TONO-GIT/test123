62270a560e9b7ae369e174512a983bb4ca975d0d
Supartono Efendi <supartono2002@gmail.com>
2022-06-05 10:44:44 +0700

    created test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+
+<html>
+<body>
+	<ul>
+		<li class="item1"><a href=""></a></li>
+		<li class="item2"><a href=""></a></li>
+		<li class="item3"><a href=""></a></li>
+		<li class="item4"><a href=""></a></li>
+	</ul>
+	<ul id="nav">
+		<li class="item1"><a href="">Item 1</a></li>
+		<li class="item2"><a href="">Item 2</a></li>
+		<li class="item3"><a href="">Item 3</a></li>
+		<li class="item4"><a href="">Item 4</a></li>
+	</ul>
+
+	
+	</body>
+	</html>
